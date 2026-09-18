b28c7fdc061b26edac694e06258559ac212ec441
Anandhakumar <anandhaanavamani@gmail.com>
2026-09-18 19:43:45 +0530

    task

---
+++ C Basic/loops.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+int main(){
+    // while
+    // while(condition){ /code to execute}
+    // int a =1;
+    // while(a<=10){
+    //     printf("The value is:%d\n",a);
+    //     ++a;
+    // }
+    // while(a<100){
+    //     // a=2 3<100
+    //     // 3%3 ==0
+    //     if(a%3==0){
+    //         printf("The divisible by three is:%d\n",a);
+    //     }
+    //     ++a;
+    // }
+
+
+
+    // do while
+    // do{
+
+    // // }while(condition)
+    // int a = 1;
+    // do{
+    //     if(a%2==0){
+    //         printf("The number is even:%d\n",a);
+    //     }
+    //     else{
+    //         printf("The number is odd:%d\n",a);
+    //     }
+    //     ++a;
+    // }while(a<100);
+
+    for(int x=0;x<100;x++){
+        if(x%5==0){
+            printf("This is divisible by 5:%d\n",x);
+        }
+    }
+
+}
+
+
+// input -> starts from 1 
+// a ->1
+// 1 to 100 ->divisible by 3 only
+
+
+// loop->while
+// filter-> condition-> filter->if
+// increment +1
+
